5844057c70fbfc4cf3779faad24173de02f333ca
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-11 04:59:23 +0300

    function to handleGoodClicks

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import Content from "./Content"
-import Header from "./Header"
-import Total from "./Total"
+import { useState } from "react"
+
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // function to handleGoodClicks
+  const handleGoodClicks = () => {
+    setGood(good + 1)
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h2>Give feedback</h2>
+
+      <div>
+        <button onClick={handleGoodClicks}>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+      </div>
+
+      <h3>Statistics</h3>
+      <p>Good {good}</p>
+      <p>Neutral</p>
+      <p>Bad</p>
     </div>
   )
 }
