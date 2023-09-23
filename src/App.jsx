8c9c0fe8c01e93877150d08d350ca5a6ca0467ed
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-23 07:15:20 +0300

    courseInfo changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
-import Content from "./Content"
-import Header from "./Header"
-import Total from "./Total"
+
+const Header = ({course}) => {
+
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <>
+      <p>
+        {part.name}
+      </p>
+      <p>
+        {part.exercises}
+      </p>
+    </>
+  )
+}
+
+const Content = ({course}) => {
+  return (
+    <div>
+       {course.parts.map((part, index) => (
+        <Part key={index} part={part} />
+       ))}
+        
+    </div>
+  )
+}
+
+const Total = ({course}) => {
+  const totalExercises = course.parts.reduce(
+    (total, part) => total + part.exercises,
+    0
+  )
+  return (
+    <div>
+        <p>Number of exercises {totalExercises}</p>
+    </div>
+  )
+}
 
 const App = () => {
   const course = {
