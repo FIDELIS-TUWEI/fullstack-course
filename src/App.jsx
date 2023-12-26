98fbf4590cfe610ae56ded8c928fadf1fddb3ff7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-12-26 06:20:49 +0300

    logic to filter names implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456" },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState("");
@@ -34,10 +37,22 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  };
+
+  // Function to filter persons by name
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <input type="search" />
+      <input 
+        type="search" 
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
       <h2>Add a new</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
@@ -50,7 +65,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPersons.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
