ec97b61c1a51534398442facb15ae4b1569bde9d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-12-26 07:23:21 +0300

    fetching persons from json server with useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 // Filter Component for search Logic
@@ -65,16 +66,21 @@ Persons.propTypes = {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+
+  // Fetch data from json-server with useEffect hook
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, []);
 
   // Function to add names to phonebook
   const handleFormSubmit = (event) => {
