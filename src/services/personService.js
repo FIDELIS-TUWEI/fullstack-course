96c8d09c7bcb2aafde73a39acb64cb02a3c4a72c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-18 05:15:22 +0300

    delete person backend service logic

--- src/services/personService.js
+++ src/services/personService.js
@@ -14,4 +14,9 @@ const create = (newObject) => {
     return axios.post(baseURL, newObject);
 };
 
-export default { getAll, create };
+// Function to delete user from phonebook with id
+const deleteUser = (id) => {
+    return axios.delete(`${baseURL}/${id}`);
+}
+
+export default { getAll, create, deleteUser };
