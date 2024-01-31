6150668983f32e4af5c16edb47e359e28de628bb
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-31 05:25:25 +0300

    logic to display search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+// Search component
+const Filter = ({ search, handleSearch }) => {
+  return (
+    <div>
+      <input type='search' onChange={handleSearch} />
+    </div>
+  )
+};
+
+Filter.propTypes = {
+  search: PropTypes.string,
+  handleSearch: PropTypes.func
+}
 
 const App = () => {
+  const [search, setSearch] = useState("");
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  }
 
   return (
-    <></>
+    <div>
+      <Filter search={search} handleSearch={handleSearch} />
+    </div>
   )
 };
 
