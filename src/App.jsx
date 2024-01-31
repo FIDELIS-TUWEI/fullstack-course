d4fbe7eb0982c693c7433dc23495012e67f9fa5d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-31 10:56:24 +0300

    handle view function refactor

--- src/App.jsx
+++ src/App.jsx
@@ -22,38 +22,63 @@ Filter.propTypes = {
   handleSearch: PropTypes.func
 };
 
+// Country Info
+const CountryInfo = ({ country }) => (
+  <div>
+    <h2>{country.name.common}</h2>
+    <p>Capital City: {country.capital}</p>
+    <p>Total Area Covered: {country.area}</p>
+    <p>Total Population: {country.population}</p>
+    <p>Region: {country.region}</p>
+    <p>Sub-region: {country.subregion}</p>
+    <p>Independent: {country.independent ? "Yes" : "No"}</p>
+    <p>Landlocked: {country.landlocked ? "Yes" : "No"}</p>
+    <p>Currency: 
+      {Object.entries(country.currencies).map(([currencyCode, currencyDetails]) => (
+        <li key={currencyCode}>{`${currencyDetails.name} (${currencyDetails.symbol})`}</li>
+      ))}
+    </p>
+    <img src={`https://flagcdn.com/w320/${country.cca2.toLowerCase()}.png`} alt="Country Flag" />
+    <p>Language (s):
+      {Object.entries(country.languages).map(([languageCode, languageName]) => (
+        <li key={languageCode}>
+          {languageName}
+        </li>
+      ))}
+    </p>
+    <p>Start of the Week: {country.startOfWeek}</p>
+    <p>Timezone: {country.timezones[0]}</p>
+  </div>
+);
+
+CountryInfo.propTypes = {
+  country: PropTypes.object,
+};
+
+// Country Weather Info
+const WeatherInfo = ({ weather }) => (
+  <div>
+    <h2>Weather in: {weather.name}</h2>
+    <p>Temperature: {weather.main.temp}°C</p>
+    <p>Humidity: {weather.main.humidity}%</p>
+    <p>Weather Condition: {weather.weather[0].description}</p>
+  </div>
+);
+
+WeatherInfo.propTypes = {
+  weather: PropTypes.object
+};
+
 // Component to display countries
-const Countries = ({ filterCountries, handleView, selectedCountry }) => {
+const Countries = ({ filterCountries, handleView, selectedCountry, weather }) => {
   if (filterCountries.length > 10) {
     return <p>More than 10 countries match your search, please refine your search</p>
-  } else if (selectedCountry) {
-    const country = selectedCountry;
+  } else if (selectedCountry && weather) {
     return (
-      <div>
-        <h2>{country.name.common}</h2>
-        <p>Capital City: {country.capital}</p>
-        <p>Total Area Covered: {country.area}</p>
-        <p>Total Population: {country.population}</p>
-        <p>Region: {country.region}</p>
-        <p>Sub-region: {country.subregion}</p>
-        <p>Independent: {country.independent ? "Yes" : "No"}</p>
-        <p>Landlocked: {country.landlocked ? "Yes" : "No"}</p>
-        <p>Currency: 
-          {Object.entries(country.currencies).map(([currencyCode, currencyDetails]) => (
-            <li key={currencyCode}>{`${currencyDetails.name} (${currencyDetails.symbol})`}</li>
-          ))}
-        </p>
-        <img src={`https://flagcdn.com/w320/${country.cca2.toLowerCase()}.png`} alt="Country Flag" />
-        <p>Language (s):
-          {Object.entries(country.languages).map(([languageCode, languageName]) => (
-            <li key={languageCode}>
-              {languageName}
-            </li>
-          ))}
-        </p>
-        <p>Start of the Week: {country.startOfWeek}</p>
-        <p>Timezone: {country.timezones[0]}</p>
-      </div>
+      <>
+        <CountryInfo country={selectedCountry} />
+        <WeatherInfo weather={weather} />
+      </>
     )
   } else {
 
@@ -74,13 +99,15 @@ const Countries = ({ filterCountries, handleView, selectedCountry }) => {
 Countries.propTypes = {
   filterCountries: PropTypes.array,
   handleView: PropTypes.func,
-  selectedCountry: PropTypes.object
+  selectedCountry: PropTypes.object,
+  weather: PropTypes.object
 }
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [weather, setWeather] = useState(null)
 
   // Function to fetch countries
   useEffect(() => {
@@ -100,22 +127,34 @@ const App = () => {
     country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
   ).slice(0, 10);
 
+  // Fetch country data
+  const fetchCountryData = (name) => {
+    return countryService.getCountryByName(name);
+  };
+
+  // Fetch weather data
+  const fetchWeatherData = (city) => {
+    return countryService.getWeatherData(city)
+  }
+
   // Function to view Country details
-  const handleView = (name) => {
-    countryService
-      .getCountryByName(name)
-      .then((data) => {
-        setSelectedCountry(data);
-      })
-      .catch(error => {
-        console.log("Error:", error);
-      })
+  const handleView = async (name) => {
+    try {
+      const data = await fetchCountryData(name);
+      setSelectedCountry(data);
+
+      const weatherData = await fetchWeatherData(data.capital);
+      setWeather(weatherData);
+      console.log("Weather Data:", weatherData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 
   return (
     <div>
       <Filter searchTerm={searchTerm} handleSearch={handleSearch} />
-      <Countries filterCountries={filterCountries} handleView={handleView} selectedCountry={selectedCountry} />
+      <Countries filterCountries={filterCountries} handleView={handleView} selectedCountry={selectedCountry} weather={weather} />
     </div>
   )
 };
