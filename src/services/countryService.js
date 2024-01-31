227e1278dc3b372d45228a9af07302ff75283c26
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-31 07:26:28 +0300

    backend logic to get country by name

--- src/services/countryService.js
+++ src/services/countryService.js
@@ -3,10 +3,14 @@ const baseURL = "https://studies.cs.helsinki.fi/restcountries";
 
 // function to get countries
 const getCountries = async () => {
-    const request = axios.get(`${baseURL}/api/all`);
-    return request.then(response => {
-        return response.data
-    })
+    const res = await axios.get(`${baseURL}/api/all`);
+    return res.data
 };
 
-export default { getCountries };
+// Function to get country by name
+const getCountryByName = async (name) => {
+    const res = await axios.get(`${baseURL}/api/name/${name}`);
+    return res.data;
+}
+
+export default { getCountries, getCountryByName };
