3c8731654e2de17eff0db7f62bbb9cb51ceaafbc
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-27 10:23:05 +0300

    baseUrl path refactor

--- src/services/personService.js
+++ src/services/personService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "/api/notes";
+const baseURL = "http://localhost:3001/api/persons";
 
 // function to get all persons from json-server
 const getAll = () => {
