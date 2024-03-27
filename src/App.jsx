a0755a22afd90579771e640065bab64944463dc1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-27 10:22:52 +0300

    logic to display validation error messages from backend added

--- src/App.jsx
+++ src/App.jsx
@@ -186,10 +186,8 @@ const App = () => {
         }, 5000);
         }
     } catch (error) {
-      console.error("Error creating new person: ", error);
-      setErrorMessage(
-        `An Error occured when creating ${newName}`
-      );
+      console.error("Error creating new person: ", error.response.data.errors);
+      setErrorMessage(error.response.data.errors);
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000);
